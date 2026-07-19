d0a4535055b9ec07b30d4a518718af8ae5988d57
firman <firman.taufik@bni.co.id>
2026-07-19 20:22:17 +0700

    add module hilt

---
+++ core-module/src/main/java/com/guide/core/module/NetworkModule.kt
@@ -0,0 +1,20 @@
+package com.guide.core.module
+
+
+import com.guide.core_api.ApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import jakarta.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideApiService(): ApiService {
+        return ApiService()
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,3 +25,4 @@ dependencyResolutionManagement {
 rootProject.name = "HomeFixGuide"
 include(":app")
 include(":core-api")
+include(":core-module")
